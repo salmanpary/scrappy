b4f679316c512e9e132a8bbdef1de9a539012bfa
Vishnurr2k01 <vishnurr2k01@gmail.com>
2022-11-05 23:44:14 +0530

    collecotr

--- pages/collector/index.js
+++ pages/collector/index.js
@@ -4,8 +4,8 @@ import Collector from '../../components/Cards/Collector'
 function CollectorHome() {
   return (
     <div>
-
-        <div className="flex flex-col gap-4 items-center">
+    <h3 className='ml-4 font-bold text-2xl'>Requests</h3>
+        <div className="flex flex-col gap-4 text-white mt-4 items-center">
         <Collector />
         <Collector />
         <Collector />
