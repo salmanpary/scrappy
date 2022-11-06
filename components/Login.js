49682272cfb872a4184934f18a427e628c00232a
Amjad Ali <cowboyamjad@gmail.com>
2022-11-06 13:59:51 +0530

    tidy up

--- components/Login.js
+++ components/Login.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { userRoleActions } from "../store/userrole-slice";
 import { useDispatch } from "react-redux";
@@ -6,63 +6,69 @@ import { useSelector } from "react-redux";
 import { useRouter } from "next/router";
 import Balls from "./Balls";
 const Login = () => {
-  const router=useRouter()
-  const dispatch=useDispatch()
-  const role=useSelector(state=>state.user.userrole)
-  const [data,setdata]=useState({
-    email:"",
-    password:""
-  })
-  const onsubmit=async()=>{
-    try{
-      const res=await axios.post("https://scrappy-beta.herokuapp.com/auth/login",data)
-      dispatch(userRoleActions.setUserRole({
-        userrole:res?.data?.user?.userrole,
-        name:res?.data?.user?.name,
-        location:res?.data?.user?.address,
-      }))
-      console.log(res)
-      if(role==="user"){
-        router.push("/userhome")
-      }else if(role==="collector"){
-        router.push("/collector")
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const role = useSelector((state) => state.user.userrole);
+  const [data, setdata] = useState({
+    email: "",
+    password: "",
+  });
+  const onsubmit = async () => {
+    try {
+      const res = await axios.post(
+        "https://scrappy-beta.herokuapp.com/auth/login",
+        data
+      );
+      dispatch(
+        userRoleActions.setUserRole({
+          userrole: res?.data?.user?.userrole,
+          name: res?.data?.user?.name,
+          location: res?.data?.user?.address,
+        })
+      );
+      console.log(res);
+      if (role === "user") {
+        router.push("/userhome");
+      } else if (role === "collector") {
+        router.push("/collector");
+      } else {
+        router.push("/muncipality");
       }
-      else{
-        router.push("/muncipality")
-      }
-    }catch(e){
-      console.log(e)
+    } catch (e) {
+      console.log(e);
     }
-
-  }
+  };
   return (
     <div className="flex-col flex items-center  gap-8 mt-12">
       <div className="card flex-col justify-center flex items-center h-screen  ">
-      <div className=" text-white font-poppins font-semibold text-2xl mt-2 mb-4">
-        Login{" "}
-      </div>
-      <div className="flex-col flex gap-4">
-        <input
-          type="text "
-          placeholder="Email Address"
-          className=" text-field  p-4 h-14 w-64 rounded-3xl bg-slate-100"
-          onChange={(e)=>setdata({...data,email:e.target.value})}
-        ></input>
+        <div className=" text-white font-poppins font-semibold text-2xl mt-2 mb-4">
+          Login{" "}
+        </div>
+        <div className="flex-col flex gap-4">
+          <input
+            type="text "
+            placeholder="Email Address"
+            className=" text-field  p-4 h-14 w-64 rounded-3xl bg-slate-100"
+            onChange={(e) => setdata({ ...data, email: e.target.value })}
+          ></input>
 
-        <input
-          type="text "
-          placeholder="Password"
-          className=" text-field  p-4 h-14 w-64 rounded-3xl bg-slate-100"
-          onChange={(e)=>setdata({...data,password:e.target.value})}
-        ></input>
+          <input
+            type="text "
+            placeholder="Password"
+            className=" text-field  p-4 h-14 w-64 rounded-3xl bg-slate-100"
+            onChange={(e) => setdata({ ...data, password: e.target.value })}
+          ></input>
+        </div>
+        <button
+          className="button-gradient text-white font-poppins   w-64  font-medium h-14 rounded-3xl mt-8"
+          onClick={() => {
+            onsubmit();
+          }}
+        >
+          Login
+        </button>
       </div>
-      <button className="button-gradient text-white font-poppins   w-64  font-medium h-14 rounded-3xl mt-8" onClick={()=>{
-        onsubmit()
-      }} >
-        Login
-      </button>
-      </div>
-      <Balls/>
+      <Balls />
     </div>
   );
 };
--- components/Muncipality/Muncipalitycard.js
+++ components/Muncipality/Muncipalitycard.js
@@ -1,32 +1,37 @@
 import axios from "axios";
 import React from "react";
 import { useState } from "react";
-import {BsChevronCompactDown , BsChevronCompactUp} from 'react-icons/bs'
+import { BsChevronCompactDown, BsChevronCompactUp } from "react-icons/bs";
 
 const Muncipalitycard = ({ item, key, resolve }) => {
-  const id = item?._id
-  const [toggle,setToggle] = useState(false)
+  const id = item?._id;
+  const [toggle, setToggle] = useState(false);
   const [accdata, setAccdata] = useState({
     status: false,
     accepted: false,
-    resolved: false
-
-  })
+    resolved: false,
+  });
   const changeData = async () => {
     try {
-      const response = await axios.post(`https://scrappy-beta.herokuapp.com/complaints/${id}`, accdata)
-      console.log(response)
+      const response = await axios.post(
+        `https://scrappy-beta.herokuapp.com/complaints/${id}`,
+        accdata
+      );
+      console.log(response);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-    console.log(accdata)
-
-  }
+    console.log(accdata);
+  };
 
   return (
-    <div style={{ 
-      backgroundImage:`url(${item?.photo})`,
-    }} className="h-[30vh] flex flex-col justify-end rounded-lg">
+    <div
+      style={{
+        backgroundImage: `url(${item?.photo})`,
+        boxShadow:'inset 0 0 0 100px rgba(0,0,0,.2)'
+      }}
+      className="h-[30vh] flex flex-col justify-end rounded-3xl"
+    >
       {/* <img src={item.photo} alt="" height={100} width={300} style={
         {
           width:'80vw',
@@ -34,37 +39,59 @@ const Muncipalitycard = ({ item, key, resolve }) => {
         }
       } /> */}
 
-      <div className="p-3 bg-slate-500 mt-[-2px] z-50 rounded-md">
-      <div className="flex justify-between ">
-        <div>{item.location} <br />
-        raised on {item.date}
-        
+      <div className="p-3 card2 mt-[-2px] z-50 rounded-3xl ">
+        <div className="flex justify-between capitalize ">
+          <div className="font-poppins font-semibold text-xl">
+            {item.location} <br />
+            <span className="font-poppins font-normal text-sm ">
+              raised on {item.date}
+            </span>
+          </div>
+          {resolve ? (
+            <div className="bg-red-400 py-2 px-4">Mark as Resolved</div>
+          ) : (
+            <div className="flex flex-col gap-3">
+              <button
+                className="button-gradient rounded-xl p-2 text-sm font-poppins text-white font-semibold"
+                onClick={() => {
+                  setAccdata({
+                    status: true,
+                    accepted: true,
+                    resolved: false,
+                  });
+                  changeData();
+                }}
+              >
+                ACCEPT
+              </button>
+              <button
+                className="button-gradient2 rounded-xl p-2 text-sm  text-white font-poppins font-semibold"
+                onClick={() => {
+                  setAccdata({
+                    status: true,
+                    accepted: false,
+                    resolved: false,
+                  });
+                  changeData();
+                }}
+              >
+                DECLINE
+              </button>
+            </div>
+          )}
         </div>
-        {resolve ? <div className="bg-red-400 py-2 px-4">
-          Mark as Resolved
-        </div> : <div className="flex flex-col gap-3">
-          <button className="bg-green-400" onClick={() => {
-            setAccdata({
-              status: true,
-              accepted: true,
-              resolved: false
-            })
-            changeData()
-          }}>ACCEPT</button>
-          <button className="bg-red-300"
-            onClick={() => {
-              setAccdata({
-                status: true,
-                accepted: false,
-                resolved: false
-              })
-              changeData()
-            }}>DECLINE</button>
-        </div>}
-       
-      </div>
-      {toggle && <>{item.title}</>}
-      <h2 className="text-white flex items-center justify-center text-3xl text-center">{toggle ? <div onClick={()=>setToggle(!toggle)}><BsChevronCompactUp /></div>:<div onClick={()=>setToggle(!toggle)}><BsChevronCompactDown /></div>}</h2>
+        {toggle && <><span className="text-poppins text-sm text-gray-700 font-semibold">{item.title}</span></>}
+        <h2 className="text-black flex items-center justify-center text-2xl pt-2 text-center">
+          {toggle ? (
+            <div onClick={() => setToggle(!toggle)}>
+              <BsChevronCompactUp  />
+            </div>
+          ) : (
+            <div onClick={() => setToggle(!toggle)}>
+              <BsChevronCompactDown  />
+            </div>
+          )}
+        </h2>
       </div>
     </div>
   );
